9282639ccd6bfe7118be9afb277d0595f95150b3
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2024-01-07 22:32:33 +0100

    creating a search bar to search for thr desire images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import ImagesCard from "./components/ImagesCard";
+import ImagesSearch from "./components/ImagesSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,11 +17,15 @@ function App() {
         setIsLoading(false);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <>
-      {isLoading ? <h1 className="loading">Loading...</h1> : <div className="grid">
+    <ImagesSearch searchText={(text) => setTerm(text)}/>
+
+    {!isLoading && images.length === 0 && <h1 className="loading">No Images Found!</h1>} 
+      
+    {isLoading ? <h1 className="loading">Loading...</h1> : <div className="grid">
       {images.map(image => (
         <ImagesCard key={image.id} image={image} />
       ))}
